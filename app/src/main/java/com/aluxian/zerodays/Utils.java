b96117b5cfaab7593eac44f908a32a17b4dbed93
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-01-23 16:33:12 +0200

    Fixes shadows and overscroll colour on 5.0

--- app/src/main/java/com/aluxian/zerodays/Utils.java
+++ app/src/main/java/com/aluxian/zerodays/Utils.java
@@ -6,17 +6,16 @@ import android.widget.TextView;
 
 public class Utils {
 
-    public static void initShadow(TextView view, AttributeSet attrs) {
-        TypedArray a = view.getContext().obtainStyledAttributes(attrs, R.styleable.TextViewShadowDips);
-        float shadowRadius = a.getDimension(R.styleable.TextViewShadowDips_shadowRadius, 0.001f);
-        float shadowDx = a.getDimension(R.styleable.TextViewShadowDips_shadowDx, 0f);
-        float shadowDy = a.getDimension(R.styleable.TextViewShadowDips_shadowDy, 0f);
+    public static void initShadow(TextView textView, AttributeSet attrs) {
+        TypedArray a = textView.getContext().obtainStyledAttributes(attrs, R.styleable.TextViewShadowDips);
         int shadowColor = a.getColor(R.styleable.TextViewShadowDips_shadowColor, 0);
 
         if (shadowColor != 0) {
-            view.setShadowLayer(shadowRadius, shadowDx, shadowDy, shadowColor);
-        } else {
-            view.getPaint().clearShadowLayer();
+            float shadowRadius = a.getDimension(R.styleable.TextViewShadowDips_shadowRadius, 1f);
+            float shadowDx = a.getDimension(R.styleable.TextViewShadowDips_shadowDx, 0f);
+            float shadowDy = a.getDimension(R.styleable.TextViewShadowDips_shadowDy, textView.getTextSize() / 12);
+
+            textView.setShadowLayer(shadowRadius, shadowDx, shadowDy, shadowColor);
         }
 
         a.recycle();
---
+++ app/src/main/java/com/aluxian/zerodays/ui/NativeNonSwipeableViewPager.java
@@ -0,0 +1,30 @@
+package com.aluxian.zerodays.ui;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NativeNonSwipeableViewPager extends ViewPager {
+
+    public NativeNonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NativeNonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Don't allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Don't allow swiping to switch between pages
+        return false;
+    }
+
+}
